86ee7844df14a03e374394e2c88519683a1f82e8
rsfragozo <rsfragozo@gmail.com>
2015-07-13 00:22:35 -0400

    OPENSHIFT CONFIGS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http'),
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-var server = http.createServer(app).listen('80', '0.0.0.0');
+var server = http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || '8080' , process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 chatServer.listen(server);
 
 app.get('/', function(req, res){
